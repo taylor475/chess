9a8534138264536dbd7ab0a9a3df4c6aa8f728eb
taylor475 <godzilla475@icloud.com>
2025-09-11 22:39:11 -0600

    feat: implement addPiece function

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -21,7 +21,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        boardLayout[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
 
     /**
