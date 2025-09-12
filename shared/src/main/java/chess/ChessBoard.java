676c2ac27e75a4936d458c72c43a85c4db3a70e4
taylor475 <godzilla475@icloud.com>
2025-09-11 22:37:32 -0600

    feat: implement getPiece function

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -32,7 +32,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return boardLayout[position.getRow() - 1][position.getColumn() - 1];
     }
 
     /**
