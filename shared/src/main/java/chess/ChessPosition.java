6c01b816e5b56a8ee4926f3bf02e7120aaff3e57
taylor475 <godzilla475@icloud.com>
2025-09-11 22:30:55 -0600

    feat: implement getCol function

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -29,6 +29,6 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return this.col;
     }
 }
