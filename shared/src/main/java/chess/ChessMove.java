a18fc87f154aba56bf3b76174cc5fcf41f246f81
taylor475 <godzilla475@icloud.com>
2025-09-13 16:34:38 -0600

    feat: implement getPromotionPiece function

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -40,6 +40,6 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return this.promotionPiece;
     }
 }
