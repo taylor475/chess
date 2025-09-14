967ff189d5403a6571763fca4ef9c7acc5fe62a2
taylor475 <godzilla475@icloud.com>
2025-09-13 18:10:02 -0600

    feat: create RookMoveCalculator

---
+++ shared/src/main/java/chess/pieceMoves/RookMoveCalculator.java
@@ -0,0 +1,15 @@
+package chess.pieceMoves;
+
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.HashSet;
+
+public class RookMoveCalculator implements MoveCalculator {
+
+    public static HashSet<ChessMove> getMoves(ChessBoard board, ChessPosition currentPosition) {
+        int[][] moveDirections = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        return MoveCalculator.generateLongMoves(currentPosition, moveDirections, board);
+    }
+}
