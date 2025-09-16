b080d0a096352d7895f53587587a78b6792bcd38
taylor475 <godzilla475@icloud.com>
2025-09-15 19:30:51 -0600

    feat: implement KnightMoveCalculator

---
+++ shared/src/main/java/chess/pieceMoves/KnightMoveCalculator.java
@@ -0,0 +1,15 @@
+package chess.pieceMoves;
+
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.HashSet;
+
+public class KnightMoveCalculator implements MoveCalculator {
+
+    public static HashSet<ChessMove> getMoves(ChessBoard board, ChessPosition currentPosition) {
+        int[][] relativeMoves = {{-1, 2}, {1, 2}, {1, -2}, {-1, -2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1}};
+        return MoveCalculator.generateShortMoves(currentPosition, relativeMoves, board);
+    }
+}
