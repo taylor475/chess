0be0a7632404ee592ea4d757756580327c0fa40c
taylor475 <godzilla475@icloud.com>
2025-09-30 18:27:17 -0600

    feat implement getTeamTurn

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -22,7 +22,7 @@ public class ChessGame {
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return teamTurn;
     }
 
     /**
