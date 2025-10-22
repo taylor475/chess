c2b0cf9556aed3709167807c1651d772fdb814ef
taylor475 <godzilla475@icloud.com>
2025-10-22 17:24:28 -0600

    feat: implement register method in UserHandler

--- server/src/main/java/server/UserHandler.java
+++ server/src/main/java/server/UserHandler.java
@@ -1,5 +1,12 @@
 package server;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import dataaccess.BadRequestException;
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+import model.AuthData;
+import model.UserData;
 import service.UserService;
 
 public class UserHandler {
@@ -7,5 +14,23 @@ public class UserHandler {
 
     public UserHandler(UserService userService) {
         this.userService = userService;
+    }
+
+    public Object register(Context ctx) throws BadRequestException {
+        UserData userData = new Gson().fromJson(ctx.body(), UserData.class);
+
+        if (userData.username() == null || userData.password() == null) {
+            throw new BadRequestException("Missing username/password");
+        }
+
+        try {
+            AuthData authData = userService.createUser(userData);
+            ctx.status(HttpStatus.OK);
+            ctx.contentType("application/json");
+            return new Gson().toJson(authData);
+        } catch (BadRequestException e) {
+            ctx.status(HttpStatus.FORBIDDEN);
+            return "{ \"message\": \"Error: username already in use\" }";
+        }
     }
 }
