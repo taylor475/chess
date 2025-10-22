8103bfcd31c9890a14f485bb667fe60aa9342f2c
taylor475 <godzilla475@icloud.com>
2025-10-21 22:50:55 -0600

    feat: create UserDAO interface

---
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -0,0 +1,13 @@
+package dataaccess;
+
+import model.UserData;
+
+public interface UserDAO {
+    UserData getUser(String username) throws DataAccessException;
+
+    void createUser(UserData user) throws DataAccessException;
+
+    boolean authenticateUser(String username, String password) throws DataAccessException;
+
+    void clear();
+}
