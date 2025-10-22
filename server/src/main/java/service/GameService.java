013f2d36eab12eb12769d459f923028682780009
taylor475 <godzilla475@icloud.com>
2025-10-21 23:58:13 -0600

    feat: implement listGames method

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,7 +1,12 @@
 package service;
 
 import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
 import dataaccess.GameDAO;
+import dataaccess.UnauthorizedException;
+import model.GameData;
+
+import java.util.HashSet;
 
 public class GameService {
     private GameDAO gameDAO;
@@ -10,5 +15,14 @@ public class GameService {
     public GameService(GameDAO gameDAO, AuthDAO authDAO) {
         this.gameDAO = gameDAO;
         this.authDAO = authDAO;
+    }
+
+    public HashSet<GameData> listGames(String authToken) throws UnauthorizedException {
+        try {
+            authDAO.getAuth(authToken);
+        } catch (DataAccessException e) {
+            throw new UnauthorizedException("No auth found: " + authToken);
+        }
+        return gameDAO.listGames();
     }
 }
