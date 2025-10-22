0386903748bb9afd270e7dd6694de28df1a5ca44
taylor475 <godzilla475@icloud.com>
2025-10-22 17:35:15 -0600

    feat: implement listGames method in GameHandler

--- server/src/main/java/server/GameHandler.java
+++ server/src/main/java/server/GameHandler.java
@@ -1,11 +1,26 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.UnauthorizedException;
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+import model.GameData;
 import service.GameService;
+
+import java.util.HashSet;
 
 public class GameHandler {
     private GameService gameService;
 
     public GameHandler(GameService gameService) {
         this.gameService = gameService;
+    }
+
+    public Object listGames(Context ctx) throws UnauthorizedException {
+        String authToken = ctx.header("authorization");
+        HashSet<GameData> games = gameService.listGames(authToken);
+
+        ctx.status(HttpStatus.OK);
+        return new Gson().toJson(games);
     }
 }
