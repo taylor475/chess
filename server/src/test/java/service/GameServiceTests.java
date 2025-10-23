42db0aeeca55a23fcc56f0665d5be845519e1441
taylor475 <godzilla475@icloud.com>
2025-10-23 00:44:34 -0600

    test: create GameServiceTests setup

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -1,4 +1,30 @@
 package service;
 
+import dataaccess.*;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+
 public class GameServiceTests {
+    private GameDAO gameDAO;
+    private AuthDAO authDAO;
+    private UserDAO userDAO;
+
+    private GameService gameService;
+    private UserService userService;
+
+    private String tokenA;
+    private String tokenB;
+
+    @BeforeEach
+    void setup() throws Exception {
+        gameDAO = new MemoryGameDAO();
+        authDAO = new MemoryAuthDAO();
+        userDAO = new MemoryUserDAO();
+
+        gameService = new GameService(gameDAO, authDAO);
+        userService = new UserService(userDAO, authDAO);
+
+        tokenA = userService.createUser(new UserData("taylor", "12345", "a@a.a")).authToken();
+        tokenB = userService.createUser(new UserData("rolyat", "54321", "b@b.b")).authToken();
+    }
 }
