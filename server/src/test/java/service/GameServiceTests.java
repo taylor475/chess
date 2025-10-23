9fb6b9b479523ddeea83faf6b66db549f83be3d1
taylor475 <godzilla475@icloud.com>
2025-10-23 00:47:39 -0600

    test: create positive listGames test

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -1,8 +1,15 @@
 package service;
 
 import dataaccess.*;
+import model.GameData;
 import model.UserData;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GameServiceTests {
     private GameDAO gameDAO;
@@ -26,5 +33,17 @@ public class GameServiceTests {
 
         tokenA = userService.createUser(new UserData("taylor", "12345", "a@a.a")).authToken();
         tokenB = userService.createUser(new UserData("rolyat", "54321", "b@b.b")).authToken();
+    }
+
+    // listGames tests
+
+    @Test
+    void listGames_success() throws Exception {
+        int id1 = gameService.createGame(tokenA, "g1");
+        int id2 = gameService.createGame(tokenA, "g2");
+        HashSet<GameData> games = gameService.listGames(tokenA);
+        assertNotNull(games);
+        assertTrue(games.stream().anyMatch(g -> g.gameID() == id1));
+        assertTrue(games.stream().anyMatch(g -> g.gameID() == id2));
     }
 }
