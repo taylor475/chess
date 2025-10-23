666ab4b6ceabc1084ae8d2dee9844748b1814fc2
taylor475 <godzilla475@icloud.com>
2025-10-23 00:13:36 -0600

    feat: create setup function for UserServiceTests

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -1,4 +1,20 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
+import dataaccess.UserDAO;
+import org.junit.jupiter.api.BeforeEach;
+
 public class UserServiceTests {
+    private UserDAO userDAO;
+    private AuthDAO authDAO;
+    private UserService userService;
+
+    @BeforeEach
+    void setup() {
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        userService = new UserService(userDAO, authDAO);
+    }
 }
