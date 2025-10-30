5be6c482fa86b9d4da6cf56c27e4bfb5701a2274
taylor475 <godzilla475@icloud.com>
2025-10-30 00:44:52 -0600

    feat: implement MySqlAuthDAO class

---
+++ server/src/main/java/dataaccess/MySqlAuthDAO.java
@@ -0,0 +1,116 @@
+package dataaccess;
+
+import com.google.gson.Gson;
+import model.AuthData;
+
+import java.sql.*;
+import java.util.HashSet;
+
+import static java.sql.Types.NULL;
+
+public class MySqlAuthDAO implements AuthDAO {
+    private HashSet<AuthData> db;
+
+    public MySqlAuthDAO() throws DataAccessException {
+        configureDatabase();
+    }
+
+    @Override
+    public void addAuth(AuthData authData) {
+        try {
+            String statement = "INSERT INTO auth (username, token) VALUES (?, ?)";
+            String jsonStatement = new Gson().toJson(authData);
+            int id = executeUpdate(statement, authData.username(), authData.authToken(), jsonStatement);
+        } catch (DataAccessException _) {
+        }
+    }
+
+    @Override
+    public void deleteAuth(String authToken) {
+        try {
+            String statement = "DELETE FROM auth WHERE token=?";
+            executeUpdate(statement, authToken);
+        } catch (DataAccessException _) {
+        }
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            String statement = "SELECT username, token FROM auth WHERE token=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1, authToken);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        String username = rs.getString("username");
+                        String token = rs.getString("token");
+                        return new AuthData(token, username);
+                    }
+                }
+            }
+        } catch (DataAccessException | SQLException e) {
+            throw new DataAccessException(String.format("Auth token does not exist: %s", authToken));
+        }
+        return null;
+    }
+
+    @Override
+    public void clear() {
+        try {
+            String statement = "TRUNCATE auth";
+            executeUpdate(statement);
+        } catch (DataAccessException _) {
+        }
+    }
+    
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)) {
+                for (int i = 0; i < params.length; i++) {
+                    Object param = params[i];
+                    if (param instanceof String p) {
+                        ps.setString(i + 1, p);
+                    }
+                    else if (param instanceof Integer p) {
+                        ps.setInt(i + 1, p);
+                    } else if (param == null) {
+                        ps.setNull(i + 1, NULL);
+                    }
+                }
+                ps.executeUpdate();
+
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new DataAccessException(String.format("Unable to update database: %s", e.getMessage()));
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS auth (
+                username VARCHAR(255) NOT NULL,
+                token VARCHAR(36) NOT NULL,
+                PRIMARY KEY (token)
+            )
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", e.getMessage()));
+        }
+    }
+}
