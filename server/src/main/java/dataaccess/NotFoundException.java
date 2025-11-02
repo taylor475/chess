b85611c2c85eb976de8fbc51af1cd502070e203e
taylor475 <godzilla475@icloud.com>
2025-11-01 18:14:57 -0600

    feat: create new NotFoundException class

---
+++ server/src/main/java/dataaccess/NotFoundException.java
@@ -0,0 +1,13 @@
+package dataaccess;
+
+/**
+ * Indicates the requested data could not be found
+ */
+public class NotFoundException extends Exception {
+    public NotFoundException(String message) {
+        super(message);
+    }
+    public NotFoundException(String message, Throwable ex) {
+        super(message, ex);
+    }
+}
