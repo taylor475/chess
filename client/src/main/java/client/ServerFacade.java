657f9cbdf70e0b74ab25ce73176f5ccc81bfbc10
taylor475 <godzilla475@icloud.com>
2025-11-11 23:47:25 -0700

    feat: initialize ServerFacade

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,4 +1,16 @@
 package client;
 
 public class ServerFacade {
+    HttpCommunicator httpComm;
+    String serverDomain;
+    String authToken;
+
+    public ServerFacade() throws Exception {
+        this("localhost:8080");
+    }
+
+    public ServerFacade(String serverDomain) throws Exception {
+        this.serverDomain = serverDomain;
+        httpComm = new HttpCommunicator(this, serverDomain);
+    }
 }
