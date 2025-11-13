4e9091059c3ff84b095ed8d882b00f2601fe1293
taylor475 <godzilla475@icloud.com>
2025-11-13 01:32:06 -0700

    feat: create PreloginRepl class

---
+++ client/src/main/java/ui/PreloginRepl.java
@@ -0,0 +1,91 @@
+package ui;
+
+import client.ServerFacade;
+
+import java.util.Scanner;
+
+import static java.lang.System.out;
+import static ui.EscapeSequences.RESET_BG_COLOR;
+import static ui.EscapeSequences.RESET_TEXT_COLOR;
+
+public class PreloginRepl {
+    ServerFacade server;
+    PostloginRepl postloginRepl;
+
+    public PreloginRepl(ServerFacade server) {
+        this.server = server;
+        postloginRepl = new PostloginRepl(server);
+    }
+
+    public void run() {
+        boolean loggedIn = false;
+        out.print(RESET_TEXT_COLOR + RESET_BG_COLOR);
+        out.println("Welcome to Chess! Enter 'help' to get started.");
+        while (!loggedIn) {
+            String[] input = getUserInput();
+            switch (input[0]) {
+                case "quit":
+                    return;
+                case "help":
+                    printHelpMenu();
+                    break;
+                case "login":
+                    if (input.length != 3) {
+                        out.println("Please provide a username and password.");
+                        printLoginInstr();
+                        break;
+                    }
+                    if (server.login(input[1], input[2])) {
+                        out.println("You are now logged in.");
+                        printLoginInstr();
+                        break;
+                    } else {
+                        out.println("Username or password incorrect, please try again.");
+                        printLoginInstr();
+                        break;
+                    }
+                case "register":
+                    if (input.length != 4) {
+                        out.println("Please provide a username, password, and email.");
+                        printRegisterInstr();
+                        break;
+                    }
+                    if (server.register(input[1], input[2], input[3])) {
+                        out.println("You are now registered and logged in.");
+                        printRegisterInstr();
+                        break;
+                    } else {
+                        out.println("Username already in use, please select a different username.");
+                        printRegisterInstr();
+                        break;
+                    }
+                default:
+                    out.println("Command not recognized, please try again.");
+                    printHelpMenu();
+                    break;
+            }
+        }
+        postloginRepl.run();
+    }
+
+    private String[] getUserInput() {
+        out.print("\n[LOGGED OUT] >>> ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine().split(" ");
+    }
+
+    private void printHelpMenu() {
+        printRegisterInstr();
+        printLoginInstr();
+        out.println("quit - stop playing");
+        out.println("help - show this menu");
+    }
+
+    private void printRegisterInstr() {
+        out.println("register <USERNAME> <PASSWORD> <EMAIL> - create a new user");
+    }
+
+    private void printLoginInstr() {
+        out.println("login <USERNAME> <PASSWORD> - login to an existing user");
+    }
+}
