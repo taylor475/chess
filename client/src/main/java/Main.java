0540c03072076826c717374e48994f5015b3bbf7
taylor475 <godzilla475@icloud.com>
2025-11-13 20:28:39 -0700

    feat: add functionality to client Main

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,18 @@
 import chess.*;
+import client.ServerFacade;
+import ui.PreloginRepl;
+
+import static java.lang.System.out;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        out.println("♕ 240 Chess Client: " + piece);
+
+        ServerFacade server = new ServerFacade();
+
+        PreloginRepl prelogin = new PreloginRepl(server);
+        prelogin.run();
+        out.println("Exited program.");
     }
 }
