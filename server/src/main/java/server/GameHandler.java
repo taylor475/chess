573a350e6da6e8f5e237c2f64164b7af5ea05fcb
taylor475 <godzilla475@icloud.com>
2025-11-14 00:32:22 -0700

    fix: dont throw exception for null color

--- server/src/main/java/server/GameHandler.java
+++ server/src/main/java/server/GameHandler.java
@@ -65,7 +65,7 @@ public class GameHandler {
             throw new BadRequestException("Missing gameID");
         }
 
-        if (color == null || color.isBlank() || !(color.equals("WHITE") || color.equals("BLACK"))) {
+        if (color != null && color.isBlank() && !(color.equals("WHITE") || color.equals("BLACK"))) {
             throw new BadRequestException("Invalid color");
         }
 
